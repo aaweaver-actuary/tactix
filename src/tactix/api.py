9f25cc590da8691414e0cbbd7632c38c6997e6b3
= <=>
2026-01-26 12:58:04 -0500

    Revert unintended CORS credentials change

--- src/tactix/api.py
+++ src/tactix/api.py
@@ -59,7 +59,6 @@ app = FastAPI(
         Middleware(
             cast("type[object]", CORSMiddleware),
             allow_origins=["*"],
-            allow_credentials=True,
             allow_methods=["*"],
             allow_headers=["*"],
         )
